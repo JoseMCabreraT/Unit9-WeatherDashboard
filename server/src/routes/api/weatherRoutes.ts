84f2e09f96136ae486d04e12308b61be78fdbb5b
Jose Cabrera <josemcabrerat@gmail.com>
2025-01-05 07:04:01 -0700

    One more update on the weatherRoutes.ts file.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,8 +1,8 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
 
- //import HistoryService from '../../service/historyService.js';
- //import WeatherService from '../../service/weatherService.js';
+ import HistoryService from '../../service/historyService.js';
+ import WeatherService from '../../service/weatherService.js';
  import historyService from '../../service/historyService.js';
 
 // TODO: POST Request with city name to retrieve weather data
